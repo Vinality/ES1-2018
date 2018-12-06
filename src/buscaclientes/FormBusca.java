e3737f704957e904788ff24568bf2d0921abd70f
Vinícius Crepschi <vibracre@gmail.com>
2018-12-06 18:41:13 -0200

    Added comment as test to netbeans git integration

--- src/buscaclientes/FormBusca.java
+++ src/buscaclientes/FormBusca.java
@@ -152,6 +152,7 @@ public class FormBusca extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new FormBusca().setVisible(true);
+                // Astalala
             }
         });
     }
